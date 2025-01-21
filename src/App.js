56c8ee27f24843bef37de47ed44dab2fcbf78b21
Rodrigo <rrs230780@gmail.com>
2025-01-21 12:20:05 -0500

    octavo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './CSS/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Juego</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ¡Bienvenido a la aplicación de Entrenamiento!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
