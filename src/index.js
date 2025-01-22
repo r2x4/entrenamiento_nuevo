a0d52348f61d0cfa56c15b475fc00caf7f75addd
Rodrigo <rrs230780@gmail.com>
2025-01-22 16:26:59 -0500

    ultimos1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-// src/index.js (con React 18 y posterior)
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // Cambiar la importación
-import './CSS/inicio.css';  // Si tienes un archivo de estilos globales
-import { BrowserRouter } from 'react-router-dom';  // Asegúrate de importar BrowserRouter
+import ReactDOM from 'react-dom/client'; 
+import './CSS/inicio.css';  
+import { BrowserRouter } from 'react-router-dom';  
 import App from './App';  // Importa App
 
 const root = ReactDOM.createRoot(document.getElementById('root'));  // Usar createRoot en lugar de render
